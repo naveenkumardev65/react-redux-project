bc13ae3789eff8d6bfe1ed749a73ad3f3e764935
naveenkarthika <yuvannaveen65@gmail.com>
2023-09-28 18:40:47 +0530

    Fetch user data in rest api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { increment, decrement } from './store';
+import axios from 'axios';
 
 function App(props) {
   const { count, dispatch } = props;
+  const [record, setRecord] = useState(false);
+
+  async function fetchUserPosts(){
+    const res = await axios.get('https://jsonplaceholder.typicode.com/posts').then(res => res.data)
+    if(res && res?.length > 0) {
+      setRecord(res)
+    }
+  }
+
+  // Perform api calls before the component mount or inject the DOM.
+  useEffect(() => {
+    try {
+      fetchUserPosts()
+    } catch (error) {
+      console.log('error', error)
+    }
+  }, []);
+
+
   return (
     <div>
-      <h4>Count- {count}</h4>
-      <button  onClick={() => dispatch(increment(75))}>+</button>
-      <button disabled={count <= 0 ? true : false} onClick={() => dispatch(decrement(20))}>-</button>
+      {record && record?.map((e, i) => {
+        return <p key={i}>{e.title}</p>
+      })}
     </div>
   )
 }
-
-
 
 // Selector
 function getCounter(state) {
